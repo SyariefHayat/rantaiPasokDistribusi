1877f9f8416e1de91efce4b6b6857377a3e8af45
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-06-25 17:34:09 +0700

    fix: cta import

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,11 @@
 import DefaultLayout from '@/components/layouts/DefaultLayout'
-import CTASection from '@/components/modules/landing/CtaSection'
+import CTASection from '@/components/modules/landing/CTASection'
 import FeatureSection from '@/components/modules/landing/FeatureSection'
 import FooterSection from '@/components/modules/landing/FooterSection'
 import HeroSection from '@/components/modules/landing/HeroSection'
 import MarketplaceSection from '@/components/modules/landing/MarketplaceSection'
 import Navigation from '@/components/modules/landing/Navigation'
-import PricingSection from '@/components/modules/landing/PricingSection'
 import StatsSection from '@/components/modules/landing/StatSection'
-import TestimonialSection from '@/components/modules/landing/TestimonialSection'
 import React from 'react'
 
 const Landing = () => {
