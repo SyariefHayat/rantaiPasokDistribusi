79fc27dcdcf1d634858c644916abf56cdcd4dc03
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-06-26 21:09:26 +0700

    update frontend: about us ui

--- src/components/modules/landing/AboutUs.jsx
+++ src/components/modules/landing/AboutUs.jsx
@@ -1,5 +1,5 @@
-import { ArrowLeft, ArrowRight } from 'lucide-react'
 import React from 'react'
+import { ArrowRight } from 'lucide-react'
 
 const AboutUs = () => {
     const stats = [
@@ -22,9 +22,17 @@ const AboutUs = () => {
                 ))}
             </dl>
 
-            <div className="w-full">
-                <p className="text-base/7 font-semibold text-indigo-600">Deploy faster</p>
-                <h2 className="text-3xl leading-snug lg:leading-[1.3] mb-5">Di Rantai Pasok Distribusi, kami berkomitmen untuk mentransformasi dunia pertanian melalui pemanfaatan teknologi canggih dan wawasan berbasis data. Kami hadir untuk membantu petani dan pelaku agribisnis mengambil keputusan yang lebih cerdas, mengoptimalkan sumber daya, dan menerapkan praktik berkelanjutan demi kesuksesan jangka panjang.</h2>
+            <div className="w-full h-full flex flex-col justify-between gap-3">
+                <div className="w-[80%] my-auto">
+                    <p className="text-2xl leading-snug lg:leading-[1.3]">Di Rantai Pasok Distribusi, kami berkomitmen untuk mentransformasi dunia pertanian melalui pemanfaatan teknologi canggih dan wawasan berbasis data. Kami hadir untuk membantu petani dan pelaku agribisnis mengambil keputusan yang lebih cerdas, mengoptimalkan sumber daya, dan menerapkan praktik berkelanjutan demi kesuksesan jangka panjang.</p>
+                </div>
+                <div className='w-[40%] self-end'>
+                    <p className=" leading-snug lg:leading-[1.3] mb-5">Kami percaya teknologi adalah kunci masa depan pertanian. Misi kami adalah mempercepat akses pasar, pendanaan, dan solusi pintar untuk petani jagung dan pelaku agribisnis, guna meningkatkan hasil, efisiensi, dan pertumbuhan berkelanjutan.</p>
+                    <p className="flex items-center gap-3 text-gray-400 ">
+                        Lihat Lebih Detail
+                        <ArrowRight size={18}/>
+                    </p>
+                </div>
             </div>
         </section>
     )
