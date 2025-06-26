26f9d6361c175f3a4c223dd48344af7bee9847d0
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-06-26 11:24:26 +0700

    update frontend: add list navbar constant

---
+++ src/constants/listNavbar.js
@@ -0,0 +1,18 @@
+export const LIST_NAVBAR = [
+    {
+        title: "Beranda",
+        url: "/"
+    },
+    {
+        title: "Tentang Kami",
+        url: "/about-us"
+    },
+    {
+        title: "Bisnis",
+        url: "/bisnis"
+    },
+    {
+        title: "Marketplace",
+        url: "/marketplace"
+    }
+]
