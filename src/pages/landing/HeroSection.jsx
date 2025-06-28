b4b41860d7d01a2410239e0f3030a5d65231220a
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-06-28 07:46:15 +0700

    update: hero section add animation

--- src/pages/landing/HeroSection.jsx
+++ src/pages/landing/HeroSection.jsx
@@ -1,30 +1,73 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
+import { useMediaQuery } from 'react-responsive';
 
 import { Button } from '@/components/ui/button';
+import SplitText from '@/components/modules/Animation/SplitText';
+import AnimatedContent from '@/components/modules/Animation/AnimatedContent';
 
 const HeroSection = () => {
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+
     return (
         <section className="relative w-full min-h-screen bg-[url(/23.jpg)] bg-center bg-cover flex items-end p-4 sm:p-6 lg:p-8 text-white">
             <div className="absolute inset-0 bg-black/40"></div>
             <div className="h-[65%] sm:h-[60%] lg:h-[60%] w-full flex flex-col justify-between max-w-7xl mx-auto relative z-10 pb-4 sm:pb-0">
                 <div className="w-full lg:w-4/5 xl:w-3/4 mb-5 sm:mb-0">
-                    <h2 className="text-3xl sm:text-4xl lg:text-5xl leading-snug lg:leading-[1.3]">Menghubungkan petani jagung dengan teknologi cerdas dan solusi pendanaan berkelanjutan untuk masa depan pertanian yang lebih sejahtera.</h2>
+                    <SplitText
+                        text="Menghubungkan petani jagung dengan teknologi cerdas dan solusi pendanaan berkelanjutan untuk masa depan pertanian yang lebih sejahtera."
+                        className="text-3xl sm:text-4xl lg:text-5xl leading-snug lg:leading-[1.3]"
+                        delay={150}
+                        duration={0.6}
+                        ease="power3.out"
+                        splitType={isDesktop ? "lines" : "words"}
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="left"
+                    />
                 </div>
 
                 <div className="flex flex-col md:flex-row items-start md:items-end justify-between gap-6 lg:mt-8">
-                    <Button variant="outline" className="bg-white text-black rounded-full cursor-pointer group hover:bg-transparent hover:text-white">
-                        <Link to="/get-started" className="flex items-center justify-between gap-2">
-                            <span>Mulai Sekarang</span>
-                            <span className="p-1 bg-black rounded-full group-hover:bg-white transition-all">
-                                <ArrowRight size={16} className="text-white group-hover:text-black" />
-                            </span>
-                        </Link>
-                    </Button>
+                    <AnimatedContent
+                        distance={250}
+                        direction="horizontal"
+                        reverse={true}
+                        duration={1.2}
+                        ease="power3.out"
+                        initialOpacity={0.2}
+                        animateOpacity
+                        scale={1}
+                        threshold={0.1}
+                        delay={0.3}
+                    >
+                        <Button variant="outline" className="bg-white text-black rounded-full cursor-pointer group hover:bg-transparent hover:text-white">
+                            <Link to="/get-started" className="flex items-center justify-between gap-2">
+                                <span>Mulai Sekarang</span>
+                                <span className="p-1 bg-black rounded-full group-hover:bg-white transition-all">
+                                    <ArrowRight size={16} className="text-white group-hover:text-black" />
+                                </span>
+                            </Link>
+                        </Button>
+                    </AnimatedContent>
 
                     <div className="w-full md:w-1/3 text-sm text-white/80">
-                        <p>Kami hadir untuk mendukung petani dan agribisnis jagung dengan solusi inovatif, mempercepat akses ke pasar, meningkatkan produktivitas, dan mendorong pertumbuhan yang inklusif dan berkelanjutan.</p>
+                        <SplitText
+                            text="Kami hadir untuk mendukung petani dan agribisnis jagung dengan solusi inovatif, mempercepat akses ke pasar, meningkatkan produktivitas, dan mendorong pertumbuhan yang inklusif dan berkelanjutan."
+                            className=""
+                            delay={isDesktop ? 150 : 100}
+                            duration={0.6}
+                            ease="power3.out"
+                            splitType={isDesktop ? "lines" : "words"}
+                            from={{ opacity: 0, y: 40 }}
+                            to={{ opacity: 1, y: 0 }}
+                            threshold={0.1}
+                            rootMargin="-100px"
+                            textAlign={isDesktop ? "end" : "left"}
+                        />
+                        <p></p>
                     </div>
                 </div>
             </div>
--- src/pages/landing/NavBar.jsx
+++ src/pages/landing/NavBar.jsx
@@ -6,44 +6,55 @@ import EachUtils from '@/utils/EachUtils';
 import { Button } from '@/components/ui/button';
 import { LIST_NAVBAR } from '@/constants/listNavbar';
 import AccountMobile from '@/components/modules/landing/NavMobile';
+import AnimatedContent from '@/components/modules/Animation/AnimatedContent';
 
 const NavBar = () => {
     return (
         <header className="w-full p-6 lg:px-8 absolute z-10 text-white">
-            <nav className="flex items-center justify-between">
-                <a href="/" className="flex-shrink-0 mt-1">
-                    <h1 className="text-lg sm:text-xl lg:text-2xl whitespace-nowrap">
-                        <span className="hidden sm:inline">Rantai Pasok Distribusi</span>
-                        <span className="sm:hidden">RPD</span>
-                    </h1>
-                </a>
-
-                <AccountMobile />
-
-                <ul className="hidden lg:flex gap-6 xl:gap-10">
-                    <EachUtils
-                        of={LIST_NAVBAR}
-                        render={(item, index) => (
-                            <li key={index}>
-                                <a href={item.url} className="relative cursor-pointer group inline-block overflow-hidden">
-                                    {item.title}
-                                    <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
-                                </a>
-                            </li>
-                        )}
-                    />
-                </ul>
-
-                <Button
-                    variant="outline"
-                    className="hidden lg:flex bg-transparent border-1 border-white rounded-full cursor-pointer"
-                >
-                    <Link to="/contact-us" className="flex items-center gap-2">
-                        <span className="whitespace-nowrap">Hubungi Kami</span>
-                        <ArrowRight size={16} />
-                    </Link>
-                </Button>
-            </nav>
+            <AnimatedContent
+                distance={150}
+                direction="vertical"
+                reverse={true}
+                duration={1.2}
+                ease="power3.out"
+                initialOpacity={0.2}
+                animateOpacity
+                scale={1}
+                threshold={0.1}
+                delay={0.3}
+            >
+                <nav className="flex items-center justify-between">
+                    <a href="/" className="flex-shrink-0 mt-1">
+                        <h1 className="text-lg sm:text-xl lg:text-2xl whitespace-nowrap">
+                            <span className="hidden sm:inline">Rantai Pasok Distribusi</span>
+                            <span className="sm:hidden">RPD</span>
+                        </h1>
+                    </a>
+                    <AccountMobile />
+                    <ul className="hidden lg:flex gap-6 xl:gap-10">
+                        <EachUtils
+                            of={LIST_NAVBAR}
+                            render={(item, index) => (
+                                <li key={index}>
+                                    <a href={item.url} className="relative cursor-pointer group inline-block overflow-hidden">
+                                        {item.title}
+                                        <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+                                    </a>
+                                </li>
+                            )}
+                        />
+                    </ul>
+                    <Button
+                        variant="outline"
+                        className="hidden lg:flex bg-transparent border-1 border-white rounded-full cursor-pointer"
+                    >
+                        <Link to="/contact-us" className="flex items-center gap-2">
+                            <span className="whitespace-nowrap">Hubungi Kami</span>
+                            <ArrowRight size={16} />
+                        </Link>
+                    </Button>
+                </nav>
+            </AnimatedContent>
         </header>
     );
 };
