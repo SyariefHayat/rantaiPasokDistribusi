919e2552694540fbb9b3fe14f0d64ac3aa5e0f87
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-20 08:39:05 +0700

    update frontend: hero and desc text

--- src/pages/landing/HeroSection.jsx
+++ src/pages/landing/HeroSection.jsx
@@ -9,13 +9,13 @@ const HeroSection = () => {
                     <h1 className="font-Avallon text-6xl sm:text-7xl lg:text-9xl font-bold leading-tight tracking-[0.05em] sm:tracking-[0.1em] lg:tracking-[0.2em] px-2 sm:px-4 lg:px-7">
                         Selamat Datang
                     </h1>
-                    <img src="/desc-text.png" alt="" className="w-[80%] sm:w-[55%] lg:w-[35%] ml-12 sm:ml-[39.5%] lg:ml-[41.8%] mt-5" />
+                    <img src="/desc-text.png" alt="" className="w-[80%] sm:w-[60%] lg:w-[40%] ml-12 sm:ml-[39%] lg:ml-[41.8%]" />
                 </div>
 
                 <div className="flex-1 flex flex-col lg:flex-row lg:items-center gap-8 lg:gap-12 px-2 sm:px-4 lg:px-7 py-8 lg:py-0">
                     <div className="w-full lg:w-1/2 space-y-4 lg:space-y-6 order-2 lg:order-1 -mt-5 sm:-mt-16 lg:mt-0">
-                        <div className="space-y-2 lg:space-y-4 -ml-2 sm:-ml-3">
-                            <div className="w-full h-10 sm:h-14 lg:h-[70px] flex items-center justify-start">
+                        <div className="space-y-2 lg:space-y-4 -ml-2 lg:-ml-3">
+                            <div className="w-full h-10 sm:h-12 lg:h-[60px] flex items-center justify-start">
                                 <img 
                                     src="/hero-text.png" 
                                     alt="Putus Rantai Pasok" 
@@ -31,7 +31,7 @@ const HeroSection = () => {
                     </div>
 
                     <div className="w-full lg:w-1/2 relative flex items-center justify-center lg:justify-end order-1 lg:order-2 min-h-[300px] sm:min-h-[400px] lg:min-h-[500px]">
-                        <div className="relative z-10 flex items-center justify-center w-full h-full mt-3 sm:mt-0 lg:-mt-1">
+                        <div className="relative z-10 flex items-center justify-center w-full h-full mt-3 sm:mt-0 ">
                             <img
                                 src="/mockup.png"
                                 alt="mockup sijuki"
